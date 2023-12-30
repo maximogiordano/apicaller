ffa8034ea66f4a322f554c887912e17af251f1e4
Maximo Giordano <maximo.giordano@globallogic.com>
2023-12-29 22:56:51 -0300

    first commit

---
+++ src/main/java/com/mg/apicaller/ApicallerApplication.java
@@ -0,0 +1,13 @@
+package com.mg.apicaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicallerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mg/apicaller/ApicallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mg.apicaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
